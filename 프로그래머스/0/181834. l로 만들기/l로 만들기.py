52181f372a7c2592bebbcc08abdfb62c92e10ce5
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-02 20:38:20 +0900

    [level 0] Title: l로 만들기, Time: 7.81 ms, Memory: 9.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = []
+    
+    for char in myString:
+        if char < 'l':
+            answer.append("l")
+        else:
+            answer.append(char)
+    return "".join(answer)
